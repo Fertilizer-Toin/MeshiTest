de38336eff02f96d0708049a5eb53045e8dbe493
Tanaka jumpei <Nodobotoke.tanaka@gmail.com>
2020-03-23 17:10:47 +0900

    templateにした

--- akinater_food/kensaku/views.py
+++ akinater_food/kensaku/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import choices
 # Create your views here.
 def index_template(request):
-    data = choices.objects.all(),
-    params = {'message':'データ一覧','data':data}
-    return render(request,'index.html',params)
+    myapp_data = {
+      'app': 'Django',
+      }
+    return render(request,'index.html',myapp_data)
