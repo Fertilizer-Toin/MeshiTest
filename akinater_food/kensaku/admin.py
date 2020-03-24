8925211e770d362350e63cc338fffb9069a5a876
Tanaka jumpei <Nodobotoke.tanaka@gmail.com>
2020-03-24 14:49:56 +0900

    新たなるテーブルを付与

--- akinater_food/kensaku/admin.py
+++ akinater_food/kensaku/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import choices
+from .models import choices,questions
  
 admin.site.register(choices)
+admin.site.register(questions)
 # Register your models here.
--- akinater_food/kensaku/views.py
+++ akinater_food/kensaku/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from .models import choices
+import random
 # Create your views here.
+#このリストややこしいな。二次元配列です。[問題,ボタン１の解答,聞いた答えのカラムの番号。例）味=16,解答　甘み =1,ボタン２の解答,カラム,解答,ボタン３の解答,カラム,解答,ボタンの解答４カラム,解答,]
+question_data =[["果物カゴがあります。あなたはどれを選ぶ？","みかん",16,"12","カカオ",16,"3","なぜか混じってた唐辛子",16,"6","全部!",16,"5"]]
+kuji = random.randint(0,0)
+question = question_data[kuji]
 def index_template(request):
     myapp_data = {
-      'app': 'Django',
-      }
+        'app':'Django'
+    }
     return render(request,'index.html',myapp_data)
 def questions(request):
-    questions_data = {'questions':[],}
-    return render(request,'question.html',questions_data)
-def answer(request):
-    if request.method == 'PODT':
-        
-    
+    q ={'question':question}
+    return render(request,'question.html',q)
+def answer1(request):
+    answer = []
+    if request.method == 'POST':
+        if 'one' in request.POST:
+            answer[2]+=question[kuji][3]
+        elif 'two' in request.POST:
+            answer[5]+=question[kuji][3]
+        elif  'three' in request.POST:
+            answer[8]+=question[kuji][3]
+        elif 'four' in request.POST:
+            answer[9]+=question[kuji][3]
