ec04fed040f29744f8f5c9980a3de08407a0f8ad
Tanaka jumpei <Nodobotoke.tanaka@gmail.com>
2020-03-26 08:55:38 +0900

    クイズ機能実装

--- akinater_food/kensaku/urls.py
+++ akinater_food/kensaku/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
- 
+app_name = 'ans'
 urlpatterns = [
     path('templates/', views.index_template, name='index_template'),
-    path('question/',views.questions,name ='questions')
+    path('question/',views.Questions,name ='Questions'),
+    path('question/',views.answer1,name='answer1'),
 ]
--- akinater_food/kensaku/views.py
+++ akinater_food/kensaku/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
-from .models import choices
+from django.db import connection
+from .models import choices,questions
 import random
 # Create your views here.
-#このリストややこしいな。二次元配列です。[問題,ボタン１の解答,聞いた答えのカラムの番号。例）味=16,解答　甘み =1,ボタン２の解答,カラム,解答,ボタン３の解答,カラム,解答,ボタンの解答４カラム,解答,]
-question_data =[["果物カゴがあります。あなたはどれを選ぶ？","みかん",16,"12","カカオ",16,"3","なぜか混じってた唐辛子",16,"6","全部!",16,"5"]]
-kuji = random.randint(0,0)
-question = question_data[kuji]
+kuji = random.randint(1,1)
+question = questions.objects.get(pk=kuji)
 def index_template(request):
     myapp_data = {
         'app':'Django'
     }
     return render(request,'index.html',myapp_data)
-def questions(request):
-    q ={'question':question}
+def Questions(request):
+    q ={
+        'question':question.question_text,
+        'choice_1':question.answer1_text,
+        'answer1_column ':question.answer1_column,
+        'answer1_get_ans':question.answer1_get_ans,
+        'choice_2':question.answer2_text,
+        'answer2_column ':question.answer2_column,
+        'answer2_get_ans':question.answer2_get_ans,
+        'choice_3':question.answer3_text,
+        'answer3_column ':question.answer3_column,
+        'answer3_get_ans':question.answer3_get_ans,
+        'choice_4':question.answer4_text,
+        'answer4_column ':question.answer4_column,
+        'answer4_get_ans':question.answer4_get_ans,
+
+    }
     return render(request,'question.html',q)
 def answer1(request):
     answer = []
