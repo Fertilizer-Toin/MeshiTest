de2a23cef7f98fb340b7f0c893972850f3d5260a
Tanaka jumpei <Nodobotoke.tanaka@gmail.com>
2020-04-02 13:39:15 +0900

    無理だぁ

--- akinater_food/kensaku/urls.py
+++ akinater_food/kensaku/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-app_name = 'ans'
 urlpatterns = [
     path('templates/', views.index_template, name='index_template'),
     path('question/',views.Questions,name ='Questions'),
